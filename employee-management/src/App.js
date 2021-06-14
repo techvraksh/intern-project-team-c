fa52f1608b6f04d44d8d6accdd4fa4f217823280
saketh018 <saketh50@gmail.com>
2021-06-14 23:59:00 +0530

    added navbar and sidebar

--- employee-management/src/App.js
+++ employee-management/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./Front-end/components/navbar";
+import SideBar from "./Front-end/components/sidebar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Employee Management System</h1>
+      <NavBar />
+      <SideBar />
     </div>
   );
 }
